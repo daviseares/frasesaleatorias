665dbe7e53909d3199da9bedf5416abda6fb2fbe
davi.seares <davi.bseares@gmail.com>
2018-10-19 23:44:06 -0300

    atualizando arquivo index e adicionando uma logo

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image,Alert } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Image
+        source={require('./imgs/logo.png')}
+        />
+        <TouchableOpacity onPress={frasesAleatorias} style={styles.botao}>
+          <Text style={styles.text}>Nova Frase</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -18,4 +23,39 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold'
+    
+  },
+  botao: {
+    backgroundColor: '#538530',
+    padding: 20,
+    borderRadius: 10,
+    marginTop:20,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+  }
 });
+
+const frasesAleatorias = () =>{
+
+  var numeroAl = Math.random();
+  numeroAl = Math.floor(numeroAl * 6);
+
+  var frases = Array();
+  frases[0] ='Bom dia Meu Jovem!';
+  frases[1] ='Você é especial meu consagrado!';
+  frases[2] ='É hora do show!';
+  frases[3] ='GLÓRIA A DEUXX';
+  frases[4] ='Não desiste de viver, a vida tem jeito';
+  frases[5] ='Seja otimista, as coisas vão melhorar';
+  frases[6] ='Fé no Pai que o inimigo cai';
+   
+
+  Alert.alert(frases[numeroAl]);
+
+}
